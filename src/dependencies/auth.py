ba334901648eeb22240ac74bc944c9e85394a2fc
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-10-10 19:02:53 -0500

    feat: Create auth dependencies for routes

--- src/dependencies/auth.py
+++ src/dependencies/auth.py
@@ -11,7 +11,15 @@ Should we handle refreshing? idk
 """
 
 from bcrypt import hashpw, gensalt, checkpw
+from fastapi import Request, Response, HTTPException, Depends
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from os import environ
+import time
+import jwt
 
+
+JWT_SECRET = environ["JWT_SECRET"]
+ALGORITHM = environ["JWT_ALGORITHM"]
 SALT_ROUNDS = 12
 
 
@@ -25,3 +33,62 @@ def hash_password(plain_pw: str) -> str:
 
 def verify_password(plaintext_pw: str, hashed_pw: str) -> bool:
     return checkpw(plaintext_pw.encode("utf-8"), hashed_pw.encode("utf-8"))
+
+
+# Signs the JWT string
+def sign_JWT(userId: str, response: Response):
+    payload = {"userId": userId, "expiry": time.time() + 3600}
+    token = jwt.encode(payload, JWT_SECRET, algorithm=ALGORITHM)
+    response.set_cookie("access_token", token, httponly=True)
+    return response
+
+
+def decodeJWT(token: str):
+    try:
+        decoded_token = jwt.decode(token, JWT_SECRET, algorithms=[ALGORITHM])
+        return decoded_token if decoded_token["expiry"] >= time.time() else None
+    except:
+        return {}
+
+
+class JWTBearer(HTTPBearer):
+    def __init__(self, auto_Error: bool = True):
+        super(JWTBearer, self).__init__(auto_error=auto_Error)
+
+    async def __call__(self, request: Request) -> str | None:
+
+        token = request.cookies.get("access_token")
+        if not token:
+            credentials: HTTPAuthorizationCredentials | None = await super(
+                JWTBearer, self
+            ).__call__(request)
+
+            if credentials and credentials.scheme == "Bearer":
+                token = credentials.credentials
+
+        if not token:
+            raise HTTPException(status_code=403, detail="Invalid or Expired token!")
+
+        if not self.verify_jwt(token):
+            raise HTTPException(status_code=403, detail="Invalid or Expired token!")
+
+        return token
+
+    def verify_jwt(self, jwt_token: str):
+        isTokenValid = False
+        payload = decodeJWT(jwt_token)
+        if payload:
+            isTokenValid = True
+        return isTokenValid
+
+
+async def get_current_user(token: str = Depends(JWTBearer())):
+    payload = decodeJWT(token)
+    if not payload:
+        raise HTTPException(status_code=403, detail="Invalid or Expired token!")
+
+    user_id = payload.get("userId")
+    if not user_id:
+        raise HTTPException(status_code=403, detail="Invalid token payload!")
+
+    return user_id
