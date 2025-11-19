a72db42773609689527fbfb2641e1e8ff37046c8
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-11-19 17:51:41 -0600

    feat: Prepare app for staging deployment

--- src/main.py
+++ src/main.py
@@ -26,9 +26,17 @@ app.add_middleware(ErrorHandlingMiddleware)
 app.exception_handler(RequestValidationError)(validation_exception_handler)
 
 # Configure CORS
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "https://your-frontend-domain.com",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  # FIXME: CHANGE FOR PRODUCTION LATER
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
