879759647cb619907ed4fc8ce50046926b286f39
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-11-19 16:30:57 -0600

    feat: Overhaul authentication w/ new error handling

--- src/dependencies/auth.py
+++ src/dependencies/auth.py
@@ -12,11 +12,13 @@ Should we handle refreshing? idk
 
 from bcrypt import hashpw, gensalt, checkpw
 from pydantic import BaseModel
-from fastapi import Request, Response, HTTPException, Depends
+from fastapi import Request, Response, Depends
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
+from typing import Dict
 import os
 import time
 import jwt
+from ..util import error
 
 
 JWT_SECRET = os.environ.get("JWT_SECRET")
@@ -40,7 +42,9 @@ def verify_password(plaintext_pw: str, hashed_pw: str) -> bool:
 def sign_JWT_admin(userId: int, response: Response):
     payload = {"userId": userId, "exp": time.time() + 3600, "userType": "admin"}
     if JWT_SECRET is None:
-        raise HTTPException(500, "Environemnt credentials are not loaded!")
+        raise error.ExternalServiceError(
+            "Auth", "Environment credentials are not loaded"
+        )
     token = jwt.encode(payload, JWT_SECRET, algorithm=ALGORITHMS[0])
     response.set_cookie(
         "access_token",
@@ -57,7 +61,9 @@ def sign_JWT_admin(userId: int, response: Response):
 def sign_JWT_volunteer(userId: int, response: Response):
     payload = {"userId": userId, "exp": time.time() + 3600, "userType": "volunteer"}
     if JWT_SECRET is None:
-        raise HTTPException(500, "Environment credentials are not loaded!")
+        raise error.ExternalServiceError(
+            "Auth", "Environment credentials are not loaded"
+        )
     token = jwt.encode(payload, JWT_SECRET, algorithm=ALGORITHMS[0])
     response.set_cookie(
         "access_token",
@@ -74,15 +80,17 @@ def sign_JWT_volunteer(userId: int, response: Response):
 def decodeJWT(token: str):
     try:
         if JWT_SECRET is None:
-            raise HTTPException(500, "Environemnt credentials are not loaded!")
+            raise error.ExternalServiceError(
+                "Auth", "Environment credentials are not loaded"
+            )
         decoded_token = jwt.decode(token, JWT_SECRET, algorithms=ALGORITHMS)
         return decoded_token
     except jwt.ExpiredSignatureError:
-        raise HTTPException(status_code=403, detail="Token has expired!")
-    except HTTPException as exc:
-        raise exc
-    except:
-        raise HTTPException(status_code=403, detail="Bad token credentials!")
+        raise error.AuthenticationError("Token has expired - please login again")
+    except jwt.InvalidTokenError:
+        raise error.AuthenticationError("Token has incorrect signature")
+    except Exception as e:
+        raise error.AuthenticationError("Token validation failed")
 
 
 class JWTBearer(HTTPBearer):
@@ -101,19 +109,11 @@ class JWTBearer(HTTPBearer):
                 token = credentials.credentials
 
         if not token:
-            raise HTTPException(status_code=403, detail="Invalid or Expired token!")
+            raise error.AuthenticationError("No authentication token provided")
 
-        if not self.verify_jwt(token):
-            raise HTTPException(status_code=403, detail="Invalid or Expired token!")
+        payload = decodeJWT(token)
 
-        return token
-
-    def verify_jwt(self, jwt_token: str):
-        isTokenValid = False
-        payload = decodeJWT(jwt_token)
-        if payload:
-            isTokenValid = True
-        return isTokenValid
+        return payload
 
 
 class UserTokenInfo(BaseModel):
@@ -121,17 +121,12 @@ class UserTokenInfo(BaseModel):
     user_type: str
 
 
-async def get_current_user(token: str = Depends(JWTBearer())):
-    payload = decodeJWT(token)
-    if not payload:
-        raise HTTPException(status_code=403, detail="Invalid or Expired token!")
+async def get_current_user(payload: Dict = Depends(JWTBearer())):
 
     user_id: int | None = payload.get("userId")
     user_type: str | None = payload.get("userType")
     if not user_id or not user_type:
-        raise HTTPException(status_code=403, detail="Invalid token payload!")
-
-    # Create new user token info object
+        raise error.AuthenticationError("Invalid token payload")
 
     user_token_info = UserTokenInfo(user_id=user_id, user_type=user_type)
     return user_token_info
