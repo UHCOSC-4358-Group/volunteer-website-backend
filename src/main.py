4ebac30c4dfb068e821075fac77135f8695502da
rtrevizo18 <ricardotrevizo18@gmail.com>
2025-11-28 20:41:40 -0600

    fix: Fixed frontend url for CORS

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ origins = [
     "http://127.0.0.1:5173",
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://volunteer-website-39e.web.app/",
+    "https://volunteer-website-39e.web.app",
 ]
 
 app.add_middleware(
